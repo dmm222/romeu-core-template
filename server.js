75418d3635be6a3e564e1141e20e81e441f90c68
dmm222 <danielmartimullor@gmail.com>
2026-02-22 19:23:41 +0100

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => res.send("Romeu Core Template OK ✅"));
+app.get("/health", (req, res) => res.json({ ok: true }));
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Listening on ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
